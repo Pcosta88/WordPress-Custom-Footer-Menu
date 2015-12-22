e8c30ac2eae4919dccd83ca217a13957ce2a35fe
Pat Costa <Costa.patrick@gmail.com>
2015-12-21 20:35:54 -0500

    note changes

---
+++ Custom Menu Footer.php
@@ -0,0 +1,27 @@
+
+
+<?php
+
+
+/**  Name: Custom Menu Footer
+This will create a custom menu. It goes in the functions.php file **/
+function register_my_menus() {
+	register_nav_menus (
+		array(
+			'footer-menu' => __('Footer Menu')
+			)
+		);
+}
+
+add_action('init', 'register_my_menus');
+
+?>
+
+
+<?php 
+
+/** place in footer.php to init new menu **/
+wp_nav_menu ( array ('menu' => 'Footer Menu')); ?>
+
+
+<?php
